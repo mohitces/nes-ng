28e416d747c67aec7ffb91cc22a21d2979efc472
Saanvi Sharma <jhanaksharma@outlook.com>
2026-06-11 17:35:30 +0530

    ui changes

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -50,7 +50,6 @@ export class FooterComponent {
       "links": [
         { "label": "NCNE Master Program", "url": "https://www.nexpertsolutions.com/job-guarantee-courses/ncne-master-program" },
         { "label": "Master Program In Security", "url": "https://www.nexpertsolutions.com/job-guarantee-courses/master-program-in-security-online-course" },
-        { "label": "Master Program In Cloud", "url": "https://www.nexpertsolutions.com/job-guarantee-courses/multi-vendor-cloud" },
         { "label": "Integrated CCIE Enterprise", "url": "https://www.nexpertsolutions.com/job-guarantee-courses/ccna-to-ccie-enterprise" },
         { "label": "Integrated CCIE Security", "url": "https://www.nexpertsolutions.com/job-guarantee-courses/ccna-to-ccie-security" },
         { "label": "Multi-Vendor Firewall", "url": "https://www.nexpertsolutions.com/job-guarantee-courses/multi-vendor-firewall" }
@@ -60,10 +59,9 @@ export class FooterComponent {
       "title": "Popular Courses",
       "links": [
         { "label": "CCNA", "url": "https://nexpertsolutions.com/cisco/ccna-certification-training-online" },
-        { "label": "AWS Associate", "url": "https://nexpertsolutions.com/cloud/aws-associate-certification-online-training" },
-        { "label": "Azure-104", "url": "https://nexpertsolutions.com/cloud/associate-az-104-certification-online-course" },
+        { "label": "AWS Associate", "url": "#" },
+        { "label": "Azure-104", "url": "#" },
         { "label": "CCNP Enterprise", "url": "https://nexpertsolutions.com/cisco/ccnp-enterprise-online-course" },
-        { "label": "CCIE Enterprise Infrastructure", "url": "https://www.nexpertsolutions.com/cisco/ccie-enterprise-infrastructure-course-online" },
         { "label": "Network Automation", "url": "https://www.nexpertsolutions.com/cisco/network-automation-devops-online-course-training" }
       ]
     },
@@ -76,7 +74,6 @@ export class FooterComponent {
         { "label": "CCNP Service Provider Course", "url": "https://www.nexpertsolutions.com/cisco/ccnp-service-provider-online-training-institute" },
         { "label": "CCNP Data-Center Course", "url": "https://www.nexpertsolutions.com/cisco/ccnp-data-center-training-course-online" },
         { "label": "CCNP Collaboration Course", "url": "https://www.nexpertsolutions.com/cisco/ccnp-collaboration-online-training" },
-        { "label": "CCIE Enterprise Course", "url": "https://www.nexpertsolutions.com/cisco/ccie-enterprise-infrastructure-course-online" },
         { "label": "CCIE Security Course", "url": "https://www.nexpertsolutions.com/cisco/ccie-security-training-online" },
         { "label": "CCIE Service Provider Course", "url": "https://www.nexpertsolutions.com/cisco/ccie-service-provider-training-online" },
         { "label": "CCIE Data Center Course", "url": "https://www.nexpertsolutions.com/cisco/ccie-data-center-certification-online" },
@@ -96,8 +93,6 @@ export class FooterComponent {
         { "label": "CCSE Course", "url": "https://www.nexpertsolutions.com/checkpoint/ccse-online-course" },
         { "label": "PCNSE Course", "url": "https://www.nexpertsolutions.com/palo-alto/pcnse-online-course" },
         { "label": "PANORAMA Course", "url": "https://www.nexpertsolutions.com/palo-alto/panorama-certification-online-course" },
-        { "label": "F5 101 Application Fundamental Course", "url": "https://www.nexpertsolutions.com/f5/f5-101-certification-online-training-course" },
-        { "label": "F5 201 TMOS Administration Course", "url": "https://www.nexpertsolutions.com/f5/f5-201-training-course-online" },
         { "label": "301A & 301B LTM Course", "url": "https://www.nexpertsolutions.com/f5/301a-and-302b-ltm-training-online" },
         { "label": "F5 303 ASM Course", "url": "https://www.nexpertsolutions.com/f5/303-asm-online-certification" },
         { "label": "Segment Routing Course", "url": "https://www.nexpertsolutions.com/cisco/segment-routing-online-training" },
@@ -114,28 +109,22 @@ export class FooterComponent {
         { "label": "Devnet Associate Course", "url": "https://www.nexpertsolutions.com/cisco/devnet-associate-certification-course-online" },
         { "label": "Devnet Professional Course", "url": "https://www.nexpertsolutions.com/cisco/devnet-professional-certification-training-online" },
         { "label": "Devnet Expert Course", "url": "https://www.nexpertsolutions.com/cisco/devnet-expert-certification-online" },
-        { "label": "VCA Course", "url": "https://www.nexpertsolutions.com/vmware/vca-certification-course-online" },
-        { "label": "VCP Course", "url": "https://www.nexpertsolutions.com/vmware/vcp-training-online" },
-        { "label": "A+ Course", "url": "https://www.nexpertsolutions.com/comptia/a-plus-online-training-course" },
-        { "label": "N+ Course", "url": "https://www.nexpertsolutions.com/comptia/n-plus-certification-training-course" },
-        { "label": "Server+ Course", "url": "https://www.nexpertsolutions.com/comptia/server-plus-online-certification" },
-        { "label": "CASP Course", "url": "https://www.nexpertsolutions.com/comptia/casp-plus-certification-training-online" }
+        { "label": "VCA Course", "url": "#" },
+        { "label": "VCP Course", "url": "#" },
+        { "label": "A+ Course", "url": "#" },
+        { "label": "N+ Course", "url": "#" },
+        { "label": "Server+ Course", "url": "#" },
+        { "label": "CASP Course", "url": "#" }
       ]
     },
     "vendors": {
       "title": "Vendor",
-      "links": [
-        { "label": "CISCO", "url": "https://www.nexpertsolutions.com/cisco" },
-        { "label": "Checkpoint", "url": "https://www.nexpertsolutions.com/checkpoint" },
-        { "label": "Microsoft", "url": "https://www.nexpertsolutions.com/microsoft" },
-        { "label": "Juniper", "url": "https://www.nexpertsolutions.com/juniper" },
-        { "label": "VMWare", "url": "https://www.nexpertsolutions.com/vmware" },
-        { "label": "Comptia", "url": "https://www.nexpertsolutions.com/comptia" },
-        { "label": "Cloud", "url": "https://www.nexpertsolutions.com/cloud" },
-        { "label": "F5", "url": "https://www.nexpertsolutions.com/f5" },
-        { "label": "Redhat", "url": "https://www.nexpertsolutions.com/redhat-linux-course-online" },
-        { "label": "PaloAlto", "url": "https://nexpertsolutions.com/palo-alto" }
-      ]
+      "links": [{ "label": "VCA Course", "url": "#" },
+        { "label": "Palo Alto", "url": "#" },
+        { "label": "F5", "url": "#" },
+        { "label": "Cisco", "url": "#" },
+        { "label": "AWS", "url": "#" },
+        { "label": "Juniper", "url": "#" }]
     },
     "policies": {
       "links": [
@@ -157,3 +146,4 @@ export class FooterComponent {
     }
   };
 }
+
